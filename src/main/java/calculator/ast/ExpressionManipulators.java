f6e56b23203fd5661491fa83c3d629af6e740cd2
Shohei <shofk26@gmail.com>
2018-10-27 18:37:30 -0700

    update

--- src/main/java/calculator/ast/ExpressionManipulators.java
+++ src/main/java/calculator/ast/ExpressionManipulators.java
@@ -1,11 +1,14 @@
 package calculator.ast;
 
 import calculator.interpreter.Environment;
+
+import java.util.Iterator;
+
 import calculator.errors.EvaluationError;
+import datastructures.concrete.DoubleLinkedList;
 import datastructures.interfaces.IDictionary;
 import datastructures.interfaces.IList;
 import misc.exceptions.NoSuchKeyException;
-import misc.exceptions.NotYetImplementedException;
 
 /**
  * All of the public static methods in this class are given the exact same parameters for
@@ -68,38 +71,50 @@ public class ExpressionManipulators {
         return new AstNode(toDoubleHelper(env.getVariables(), exprToConvert));
     }
 
+    /**
+     * Helper method for handleToDouble method
+     * @param variables variables saved
+     * @param node node the user wants to evaluate
+     * @return evaluated double of the given node
+     */
     private static double toDoubleHelper(IDictionary<String, AstNode> variables, AstNode node) {
         // There are three types of nodes, so we have three cases. 
         if (node.isNumber()) {
             return node.getNumericValue();
         } else if (node.isVariable()) {
             try {
-                return variables.get(node.getName()).getNumericValue();
+                return toDoubleHelper(variables, variables.get(node.getName()));
             } catch (NoSuchKeyException e) {
                 throw new EvaluationError("Variable not found.");
             }
         } else {
-            // You may assume the expression node has the correct number of children.
-            // If you wish to make your code more robust, you can also use the provided
-            // "assertNodeMatches" method to verify the input is valid.
-            String name = node.getName();
+            // save operator
+            String operator = node.getName();
             IList<AstNode> children = node.getChildren();
             
-            if (name.equals("negate")) {
+            if (operator.equals("negate")) {
+                assertNodeMatches(node, "negate", 1);
                 return -toDoubleHelper(variables, children.get(0));
-            } else if (name.equals("sin")) {
+            } else if (operator.equals("sin")) {
+                assertNodeMatches(node, "sin", 1);
                 return Math.sin(toDoubleHelper(variables, children.get(0)));
-            } else if (name.equals("cos")) {
-                return Math.sin(toDoubleHelper(variables, children.get(0)));
-            } else if (name.equals("+")) {
+            } else if (operator.equals("cos")) {
+                assertNodeMatches(node, "cos", 1);
+                return Math.cos(toDoubleHelper(variables, children.get(0)));
+            } else if (operator.equals("+")) {
+                assertNodeMatches(node, "+", 2);
                 return toDoubleHelper(variables, children.get(0)) + toDoubleHelper(variables, children.get(1));
-            } else if (name.equals("-")) {
+            } else if (operator.equals("-")) {
+                assertNodeMatches(node, "-", 2);
                 return toDoubleHelper(variables, children.get(0)) - toDoubleHelper(variables, children.get(1));
-            } else if (name.equals("*")) {
+            } else if (operator.equals("*")) {
+                assertNodeMatches(node, "*", 2);
                 return toDoubleHelper(variables, children.get(0)) * toDoubleHelper(variables, children.get(1));
-            } else if (name.equals("/")) {
+            } else if (operator.equals("/")) {
+                assertNodeMatches(node, "/", 2);
                 return toDoubleHelper(variables, children.get(0)) / toDoubleHelper(variables, children.get(1));
-            } else if (name.equals("^")) {
+            } else if (operator.equals("^")) {
+                assertNodeMatches(node, "^", 2);
                 return Math.pow(toDoubleHelper(variables, children.get(0)), toDoubleHelper(variables, children.get(1)));
             }
             // when operator does not match to any of the expected ones
@@ -141,10 +156,78 @@ public class ExpressionManipulators {
         //         the current level? Or before?
 
         assertNodeMatches(node, "simplify", 1);
+        return simplifyHelper(env, node.getChildren().get(0));
 
-        // TODO: Your code here
-        throw new NotYetImplementedException();
+        
     }
+    
+    /**
+     * Helper method for handleSimplify method
+     * @param env environment settings
+     * @param node node user wants to simplify
+     * @return simplified node
+     */
+    private static AstNode simplifyHelper(Environment env, AstNode node) {
+        // set reusable variables
+        IList<AstNode> originalChildren = node.getChildren();
+        // creates children list separate from the original one
+        IList<AstNode> children = new DoubleLinkedList<AstNode>();
+        Iterator<AstNode> iter = originalChildren.iterator();
+        while (iter.hasNext()) {
+            children.add(iter.next());
+        }
+        
+        // variables
+        IDictionary<String, AstNode> variables = env.getVariables(); 
+        AstNode simplified;
+        
+        // checks one-by-one
+        if (!node.isOperation()) {
+            if (node.isVariable() && variables.containsKey(node.getName())) {
+                String name = node.getName();
+                // simplify the variable
+                simplified =  simplifyHelper(env, variables.get(name));
+                // base case 1: return the variable in numeric form
+                if (simplified.isNumber()) {
+                    return simplified;
+                } else {
+                    return variables.get(name);
+                }
+            } else {
+                // base case 2 & 3: return the node as it is
+                return node;
+            }
+        } else {
+            String operator = node.getName();
+            // if the children are not numeric, check if they can be numeric
+            if (!children.get(0).isNumber()) {
+                children.set(0, simplifyHelper(env, children.get(0)));
+            }
+            if (children.size() == 2 && !children.get(1).isNumber()) {
+                children.set(1, simplifyHelper(env, children.get(1)));
+            }
+            simplified = new AstNode(operator, children);
+            if (children.size() == 1 && children.get(0).isNumber()) {
+                AstNode child0 = children.get(0);
+                if (operator.equals("negate")) {
+                    simplified = new AstNode(-child0.getNumericValue());
+                }
+            } else if (children.size() == 2 && children.get(0).isNumber() && children.get(1).isNumber()) {
+                AstNode child0 = children.get(0);
+                AstNode child1 = children.get(1);
+                if (operator.equals("+")) {
+                    simplified = new AstNode(child0.getNumericValue() + child1.getNumericValue());
+                } else if (operator.equals("-")) {
+                    simplified = new AstNode(child0.getNumericValue() - child1.getNumericValue());
+                } else if (operator.equals("*")) {
+                    simplified = new AstNode(child0.getNumericValue() * child1.getNumericValue());
+                }
+            }
+            return simplified;
+        }
+    }
+    
+    
 
     /**
      * Accepts an Environment variable and a 'plot(exprToPlot, var, varMin, varMax, step)'
@@ -182,20 +265,45 @@ public class ExpressionManipulators {
      * @throws EvaluationError  if 'step' is zero or negative
      */
     public static AstNode plot(Environment env, AstNode node) {
+        // checks if the correct input is given
         assertNodeMatches(node, "plot", 5);
 
-        // TODO: Your code here
-        throw new NotYetImplementedException();
-
-        // Note: every single function we add MUST return an
-        // AST node that your "simplify" function is capable of handling.
-        // However, your "simplify" function doesn't really know what to do
-        // with "plot" functions (and what is the "plot" function supposed to
-        // evaluate to anyways?) so we'll settle for just returning an
-        // arbitrary number.
-        //
-        // When working on this method, you should uncomment the following line:
-        //
-        // return new AstNode(1);
+        // get children for simplicity
+        IList<AstNode> children = node.getChildren();
+        // variable name
+        String xVar = children.get(1).getName();
+        if (env.getVariables().containsKey(xVar)) {
+            throw new EvaluationError("Variable already defined.");
+        }
+        
+        // increment saved
+        double[] increments = {toDoubleHelper(env.getVariables(), children.get(2)),
+                toDoubleHelper(env.getVariables(), children.get(3)),
+                toDoubleHelper(env.getVariables(), children.get(4))};
+        
+        if (increments[0] > increments[1]) {
+            throw new EvaluationError("Minimum larger than Maximum.");
+        }
+        if (increments[2] <= 0) {
+            throw new EvaluationError("Incrementation less than or equal to zero.");
+        }
+        
+        // creates the lists of x and y variables
+        IList<Double> xVals = new DoubleLinkedList<>();
+        IList<Double> yVals = new DoubleLinkedList<>();
+        IDictionary<String, AstNode> variables = env.getVariables();
+        for (double i = increments[0]; i <= increments[1]; i += increments[2]) {
+            xVals.add(i);
+            variables.put(xVar, new AstNode(i));
+            yVals.add(toDoubleHelper(variables, children.get(0))); // throws EvaluationError
+        }
+        // clears the variable
+        variables.remove(xVar);
+        
+        // plot
+        env.getImageDrawer().drawScatterPlot("Plot", xVar, "output", xVals, yVals);
+        
+        // Returns the simplified node
+        return simplifyHelper(env, children.get(0));
     }
 }
--- src/test/java/calculator/TestExpressionManipulators.java
+++ src/test/java/calculator/TestExpressionManipulators.java
@@ -1,16 +1,18 @@
 package calculator;
 
-import org.junit.Test;
-
-import calculator.ast.AstNode;
-import calculator.ast.ExpressionManipulators;
 import calculator.interpreter.Calculator;
-import datastructures.concrete.DoubleLinkedList;
-import datastructures.concrete.dictionaries.ArrayDictionary;
 
 public class TestExpressionManipulators {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println(calc.evaluate("sin(42)"));
+
+        System.out.println(calc.evaluate("z := 2 + x"));
+        System.out.println(calc.evaluate("simplify(z)"));
+        System.out.println(calc.evaluate("x := 10"));
+        System.out.println(calc.evaluate("simplify(z)"));
+        System.out.println(calc.evaluate("y := x * 3 + sin(40)"));
+        System.out.println(calc.evaluate("x := 2"));
+        System.out.println(calc.evaluate("z"));
+        System.out.println(calc.evaluate("toDouble(y)"));
     }
 }
