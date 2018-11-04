e9bf3accb8c983578232cccbb578c72a50948944
Shohei <shofk26@gmail.com>
2018-11-03 22:44:48 -0800

    avl added

---
+++ src/AVL.java
@@ -0,0 +1,21 @@
+package datastructures.concrete.dictionaries;
+
+public class AVL<T> {
+    /** Root node */
+    private T root;
+    
+    /** Number of nodes */
+    private int nNode;
+    
+    /**
+     * Constructor: creates and initialize the AVL
+     */
+    public AVL() {
+        root = null;
+        nNode = 0;
+    }
+    
+    public void add() {
+        
+    }
+}
